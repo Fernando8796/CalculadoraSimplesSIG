cccae4d7f06c56c5f5a257909321057ae52fe4bb
José Fernando <jf8065572@gmail.com>
2024-04-14 18:48:43 -0300

    As perguntas dos números ficaram mais compactos

--- CalculadoraSimplesSIG/principal.py
+++ CalculadoraSimplesSIG/principal.py
@@ -21,69 +21,54 @@ Escolha o Operador Aritimético
 [5] Potenciação **
 [6] Porcentagem %"
 '''
-num1 = ("Qual o primeiro número para ")
-num2 = ("Qual o segundo número?\n")
 
 while True:
     escolha = input(mensagem)
 
     if escolha == "1":
+        num1 = int(input("Qual o primeiro número para somar?"))
 
-        n1 = input(num1 + "somar?\n")
-        numb1 = int(n1)
+        num2 = int(input("Qual o segundo número?"))
 
-        n2 = input(num2)
-        numb2 = int(n2)
-
-        result = soma(numb1, numb2)
+        result = soma(num1, num2)
         print(f"O resultado é {result}")
 
     elif escolha == "2":
-        n1 = input(num1 + "subtrair?\n")
-        numb1 = int(n1)
+        num1 = int(input("Qual o primeiro número para subtrair?"))
 
-        n2 = input(num2)
-        numb2 = int(n2)
+        num2 = int(input("Qual o segundo número?"))
 
         result = sub(numb1, numb2)
         print(f"O resultado é {result}")
 
     elif escolha == "3":
-        n1 = input(num1 + "multiplicar?\n")
-        numb1 = int(n1)
+        num1 = int(input("Qual o primeiro número para multiplicar?"))
 
-        n2 = input(num2)
-        numb2 = int(n2)
+        num2 = int(input("Qual o segundo número?"))
 
-        result = mult(numb1, numb2)
+        result = mult(num1, num2)
         print(f"O resultado é {result}")
 
     elif escolha == "4":
-        n1 = input(num1 + "dividir?\n")
-        numb1 = int(n1)
+        num1 = int(input("Qual o primeiro número para dividir?"))
 
-        n2 = input(num2)
-        numb2 = int(n2)
+        num2 = int(input("Qual o segundo número?"))
 
-        result = div(numb1, numb2)
+        result = mult(num1, num2)
         print(f"O resultado é {result}")
 
     elif escolha == "5":
-        n1 = input("Qual é a base?\n")
-        numb1 = int(n1)
+        num1 = int(input("Qual é a base?\n"))
 
-        n2 = input("Qual é o expoente?\n")
-        numb2 = int(n2)
+        num2 = int(input("Qual é o expoente?\n"))
 
-        result = poten(numb1, numb2)
+        result = poten(num1, num2)
         print(f"O resultado é {result}")
 
     elif escolha == "6":
-        n1 = input("Quantos porcentos?\n")
-        numb1 = int(n1)
+        num1 = int(input("Quantos porcentos?\n"))
 
-        n2 = input("De quanto?\n")
-        numb2 = int(n2)
+        num2 = int(input("De quanto?\n"))
 
         result = porc(numb1, numb2)
         print(f"O resultado é {result}")
